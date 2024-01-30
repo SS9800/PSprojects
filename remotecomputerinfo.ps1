4c6259ab8c0ea8111203b12c1d38f5645ef60d7e
Sage <82624315+SS9800@users.noreply.github.com>
2024-01-30 17:30:28 -0500

    Delete remotecomputerinfo.ps1

--- remotecomputerinfo.ps1
+++
@@ -1,6 +0,0 @@
-﻿#prompt the user on what machine they would like to connect to
-Write-Host -ForegroundColor Yellow 'Please enter the hostname of the machine you would like to connect to.'
-$remotehost = read-host
-enter-PSSession -computername $remotehost
-#once connection has been established, print some basic stats for the machine via another script on the local machine
-Invoke-Command -ComputerName $remotehost -FilePath "C:\Users\schofields\Desktop\remotecomputerinfoV3.ps1"
